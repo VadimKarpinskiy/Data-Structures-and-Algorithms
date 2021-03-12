e858eaf1b8445366f5b752f2526f5b4736bbd290
VadimKarpinskiy <karpinskiu2000@gmail.com>
2021-03-12 19:31:48 +0100

    Mistakes fixed

--- Python/Sortings/counting_sort.py
+++ Python/Sortings/counting_sort.py
@@ -1,10 +1,10 @@
-def counting_sort(arr):
+def counting_sort(arr, k):           # [0..k] - k is the upper bound of the interval of possible array elements
     n = len(arr)
     result_arr = [0] * n
-    count_arr = [0] * n
+    count_arr = [0] * k
     for i in range(n):
         count_arr[arr[i]] += 1          # counting how many unique numbers are there in an array
-    for i in range(1, n):
+    for i in range(1, k):
         count_arr[i] += count_arr[i-1]      # how many elements are less or equal than i
     for i in range(n-1, -1, -1):
         result_arr[count_arr[arr[i]] - 1] = arr[i]
@@ -12,5 +12,6 @@ def counting_sort(arr):
     return result_arr
 
 
-tab = [3, 4, 1, 0, 5, 0, 3, 1]
-print(counting_sort(tab))
+tab = [17, 33, 2, 0, 42, 2, 777, 322, 5, 15]
+k = 1000
+print(counting_sort(tab, k))
